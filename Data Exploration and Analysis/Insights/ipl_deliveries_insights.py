4add0289e667a2d763caf7b429f1e68004c372f2
saquib.nayeem@tredence.com <saquib.nayeem@tredence.com>
2023-09-19 11:44:24 +0000

    Changes in insights of ipl_deliveries

--- Data Exploration and Analysis/Insights/ipl_deliveries_insights.py
+++ Data Exploration and Analysis/Insights/ipl_deliveries_insights.py
@@ -1,5 +1,5 @@
 # Databricks notebook source
-# Define the path to the bronze data in your data lake
+# Define the path to the cleaned silver data in your data lake
 silver_data_path = "/pipelines/c8f60a52-2538-46ba-9a33-b67b68b89cca/tables/ipl_deliveries_silver"  
 
 # Read the bronze data into a DataFrame
@@ -8,6 +8,7 @@ silver_df = spark.read.format("delta").load(silver_data_path)
 display(silver_df)
 
 # COMMAND ----------
+
 
 from pyspark.sql.functions import sum, count, col
 
@@ -36,3 +37,37 @@ bowler_stats = bowler_stats.withColumn("strike_rate", (bowler_stats["deliveries"
 
 # Show the results
 display(bowler_stats)
+
+# COMMAND ----------
+
+#Bowling strike rate:
+from pyspark.sql.functions import count, when
+
+# Group the data by bowler and count deliveries and wickets
+bowler_stats = df.groupBy("bowler").agg(
+    count("delivery").alias("deliveries"),
+    count(when(df["mode_of_dismissal"].isNotNull(), 1)).alias("wickets")
+)
+
+# Calculate the bowling strike rate
+bowler_stats = bowler_stats.withColumn("strike_rate", (bowler_stats["deliveries"] / bowler_stats["wickets"]).cast("double"))
+
+# Show the results
+display(bowler_stats)
+
+# COMMAND ----------
+
+#Mode of dismissal:
+from pyspark.sql.functions import count, when, lit
+
+# Group the data by mode of dismissal and count the occurrences
+dismissal_analysis = df.groupBy("mode_of_dismissal").agg(count("*").alias("dismissal_count"))
+
+# Replace null values in the "mode_of_dismissal" column with 'not out'
+dismissal_analysis = dismissal_analysis.withColumn(
+    "mode_of_dismissal",
+    when(dismissal_analysis["mode_of_dismissal"].isNull(), lit('not out')).otherwise(dismissal_analysis["mode_of_dismissal"])
+)
+
+# Show the results
+display(dismissal_analysis)
